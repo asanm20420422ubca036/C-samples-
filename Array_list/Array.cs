388bdc979dee585605cddb5e804dab7d08d86619
Priya <lcharu377@gmail.com>
2025-09-15 18:14:43 +0530

    initial commit

---
+++ Array_list/Array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+{
+
+    enum Books { priya,kaviya,viji,banu,vaishu }
+
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Book: priya at Index of: " + (int)Books.priya);
+        Console.WriteLine("Book: kaviya at Index of: " + (int)Books.kaviya);
+        Console.WriteLine("Book: viji at Index of: " + (int)Books.viji);
+        Console.WriteLine("Book: banu at Index of: " + (int)Books.banu);
+        Console.WriteLine("Book: vaishu at Index of: " + (int)Books.vaishu);
+        string str = Books.vaishu.ToString();
+        foreach(string name in Enum.GetNames(typeof(Books)))
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ loops/loops.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace program
+{
+    class looping
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter when the loop get start");
+            int loopstart = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter when the loops get end");
+            int loopend = Convert.ToInt32(Console.ReadLine());
+            for (int i = loopstart; i <= loopend; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+                for (int j = loopend; j >= loopstart; j--)
+                {
+                    Console.WriteLine($"{j}");
+                }
+                
+            
+
+        }
+    }
+}
